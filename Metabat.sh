3696525240828c1faee6a57e01bfde2973589a91
Valter Almeida <75810880+ValterAlmeida@users.noreply.github.com>
2024-04-18 21:54:47 +1200

    Update Metabat.sh

--- Metabat.sh
+++ Metabat.sh
@@ -1,21 +1,15 @@
 # Metabat - Binning
 
 #!/bin/bash -e
-#SBATCH -A massey03212
-#SBATCH -J Metabat_CA
-#SBATCH --time 02:00:00
-#SBATCH --mem 32GB
-#SBATCH --cpus-per-task 20
-#SBATCH -e metabat.err
-#SBATCH -o metabat.out
-#SBATCH --export NONE
+#SBATCH --account       massey03212
+#SBATCH --job-name      Metabat_hh
+#SBATCH --time          01:30:00
+#SBATCH --mem           12GB
+#SBATCH --cpus-per-task 16
+#SBATCH --error         %x_%j.err
+#SBATCH --output        %x_%j.out
 
 module purge
 module load MetaBAT/2.15-GCC-11.3.0
 
-jgi_summarize_bam_contig_depths --outpqutDepth metabat.txt cattle.sorted.bam
-
-metabat2 -t $SLURM_CPUS_PER_TASK -m 1500 \
-         -i cattle.m1000.fna \
-         -a metabat.txt \
-         -o metabat
+runMetaBat.sh -t $SLURM_CPUS_PER_TASK -m 1500 hh.m1000.fna human.sorted.bam
