741dc552c8a8f8565431b72c8d57dd4f52b11576
Valter Almeida <75810880+ValterAlmeida@users.noreply.github.com>
2024-04-18 22:29:32 +1200

    Update GTDB-Tk.sh

--- GTDB-Tk.sh
+++ GTDB-Tk.sh
@@ -1,9 +1,9 @@
-# GTDB-Tk
+#GTDB-Tk - Taxonomic classification
 
 #!/bin/bash -e
 #SBATCH --account       massey03212
-#SBATCH --job-name      GTDBTk_GW
-#SBATCH --time          03:00:00
+#SBATCH --job-name      GTDB-Tk
+#SBATCH --time          05:00:00
 #SBATCH --mem           128GB
 #SBATCH --cpus-per-task 16
 #SBATCH --error         %x_%j.err
@@ -12,4 +12,4 @@
 module purge
 module load GTDB-Tk/2.1.0-gimkl-2020a-Python-3.9.9
 
-gtdbtk classify_wf -x fa --cpus 20 --genome_dir dastool_out/_DASTool_bins/ --out_dir gtdbtk_out/
+gtdbtk classify_wf -x fa --cpus $SLURM_CPUS_PER_TASK --genome_dir _DASTool_bins/ --out_dir gtdbtk_out/
